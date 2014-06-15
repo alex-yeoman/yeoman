c26d44f57368d4be64301b5d226d815f17acd30c
Alex Davis <alexd@betterfinance.me>
2014-06-14 20:19:27 -0700

    howto-webapp-rjs

---
+++ install-webapp-rjs.sh
@@ -0,0 +1,12 @@
+" start by following this : http://yeoman.io/learning/
+" then
+" nave use latest
+" npm install -g yo
+" npm install -g generator-webapp
+" yo webapp
+" npm install -g generator-
+" npm search yeoman-generator
+" npm install generator-webapp-rjs
+" yo  webapp-rjs
+"
+" 
